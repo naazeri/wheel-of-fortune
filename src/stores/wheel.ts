566dde96e29d0df85521f624f57dcbb6e07a466d
Reza Nazeri <r.nazeri.vitrin@gmail.com>
2024-08-25 09:20:59 +0330

    add data seeder

--- src/stores/wheel.ts
+++ src/stores/wheel.ts
@@ -4,7 +4,7 @@ import type { PrizeConfig } from '@/types'
 export const useWheelStore = defineStore({
   id: 'wheel',
   state: () => ({
-    items: JSON.parse(localStorage.getItem('wheelItems') ?? '[]') as PrizeConfig[]
+    items: JSON.parse(localStorage.getItem('wheelItems') ?? getSeedData()) as PrizeConfig[]
   }),
   actions: {
     addItem(item: PrizeConfig) {
@@ -53,3 +53,90 @@ export const useWheelStore = defineStore({
     }
   }
 })
+
+const getSeedData = () => {
+  const data = [
+    {
+      id: 1,
+      prizeName: 'پلی استیشن',
+      prizeNameOnWheel: 'پلی استیشن',
+      bgColor: '#45ace9',
+      color: '#ffffff',
+      probability: 2
+    },
+    {
+      id: 2,
+      prizeName: 'ماشین',
+      prizeNameOnWheel: 'ماشین',
+      bgColor: '#f6b93b',
+      color: '#ffffff',
+      probability: 1
+    },
+    {
+      id: 3,
+      prizeName: 'دوچرخه',
+      prizeNameOnWheel: 'دوچرخه',
+      bgColor: '#6d28d9',
+      color: '#ffffff',
+      probability: 2
+    },
+    {
+      id: 4,
+      prizeName: 'آیفون',
+      prizeNameOnWheel: 'آیفون',
+      bgColor: '#1e3a8a',
+      color: '#ffffff',
+      probability: 1
+    },
+    {
+      id: 5,
+      prizeName: 'کتاب خوان',
+      prizeNameOnWheel: 'کتاب خوان',
+      bgColor: '#2ecc71',
+      color: '#ffffff',
+      probability: 5
+    },
+    {
+      id: 6,
+      prizeName: 'گیفت کارت',
+      prizeNameOnWheel: 'گیفت کارت',
+      bgColor: '#c23616',
+      color: '#ffffff',
+      probability: 12
+    },
+    {
+      id: 7,
+      prizeName: 'کد تخفیف',
+      prizeNameOnWheel: 'کد تخفیف',
+      bgColor: '#365314',
+      color: '#ffffff',
+      probability: 50
+    },
+    {
+      id: 8,
+      prizeName: 'اسپیکر بلوتوث',
+      prizeNameOnWheel: 'موارد دیگر',
+      bgColor: '#d6d3d1',
+      color: '#ffffff',
+      probability: 2
+    },
+    {
+      id: 9,
+      prizeName: 'اشتراک نامحدود',
+      prizeNameOnWheel: 'اشتراک نامحدود',
+      bgColor: '',
+      color: '',
+      probability: 5
+    },
+    {
+      id: 10,
+      prizeName: 'دستگاه اسپرسو',
+      prizeNameOnWheel: 'دستگاه اسپرسو',
+      bgColor: '',
+      color: '',
+      probability: 2
+    }
+  ]
+
+  return JSON.stringify(data)
+}
